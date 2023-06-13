0b9244ce80ef2dec5499a10464210d43ada98d35
crose1121 <coreycoding1121@outlook.com>
2023-06-13 18:41:02 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "../src/img/logo.jpg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>COMING SOON!!!</h1>
+        <img src={logo} style={{ width: "100%" }} />
+      </header>
+    </div>
+  );
+}
+
+export default App;
